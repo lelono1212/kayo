cfdec7ef391e58b9d79df0b30c0f5d2e82dca850
lelono1212 <94515234+lelono1212@users.noreply.github.com>
2022-12-20 12:29:45 +0700

    Create adsku.js

---
+++ adsku.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '9c675999da1a828698d69fbe2a788d6a',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/9c675999da1a828698d69fbe2a788d6a/invoke.js"></scr' + 'ipt>');
+</script>
